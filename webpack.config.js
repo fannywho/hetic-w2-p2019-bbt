d023930bb55e64da83f89133fe379c90124741cc
SIMAM77 <souleimane.imam@hetic.net>
2017-12-25 19:52:14 +0100

    projet bbt gulp init

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: "./app/app.js",
+    output: {
+        path: "./dist",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loader: "style!css!sass"
+            }
+        ]
+    }
+};
